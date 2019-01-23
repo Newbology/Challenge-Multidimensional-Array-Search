2b5c9c6633df67746c0a0f1d183055ebbfc14e4f
kevin tauzer <kevint@kevins-MBP.home>
2019-01-23 11:58:49 -1000

    completed Multidimensional array search

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
+'use strict';
+
+function flatten(array) {
+  let tempArr = [];
+
+  for (let i = 0; i < array.length; i++) {
+    if (Array.isArray(array[i])) {
+      tempArr = tempArr.concat(flatten(array[i]));
+    } else {
+      tempArr.push(array[i]);
+    }
+  }
+  return tempArr;
+}
+
 function locate(array, target) {
   // do work here
+  return flatten(array).includes(target);
 }
 
 module.exports = locate;
